7e891099ba304c2ad975180d7611053f2af064ab
IreneOliveto <nene.ol@hotmail.it>
2022-08-23 18:14:51 +0200

    Update Lab(3.02.2).sql

--- Lab(3.02.2).sql
+++ Lab(3.02.2).sql
@@ -23,6 +23,15 @@ PRIMARY KEY (flight_number),
 FOREIGN KEY (id_aircraft) references aircraft(id_aircraft)
 );
 
+create table customers_flights(
+id_customers_flights int auto_increment,
+flight_number varchar(255),
+id_customer int,
+PRIMARY KEY (id_customers_flights),
+FOREIGN KEY (flight_number) REFERENCES flight(flight_number),
+FOREIGN KEY (id_customer) REFERENCES customer(id_customer)
+);
+
 -- 3. In the Airline database write the SQL script to get the total number of flights in the database.
 select count(*) from flight;
 
@@ -45,5 +54,16 @@ select count(*) from aircraft where aircraft_name like '%Boeing%';
 select * from flight where flight_mileage > 300 AND flight_mileage < 2000;
 
 -- 10. In the Airline database write the SQL script to find the average flight distance booked grouped by customer status (this should require a join).
+select c.customer_status, avg(f.flight_mileage) from customers_flights cf
+inner join flight f on cf.flight_number = f.flight_number
+inner join customer c on cf.id_customer = c.id_customer
+group by c.customer_status;
 
 -- 11. In the Airline database write the SQL script to find the most often booked aircraft by gold status members (this should require a join).
+select id_aircraft, count(*) from customers_flights cf
+inner join flight f on cf.flight_number = f.flight_number
+inner join customer c on cf.id_customer = c.id_customer
+inner join aircraft a on a.id_aircraft = f.id_aircraft
+where c.customer_status = 'Gold'
+group by id_aircraft;
+
